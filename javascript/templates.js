90649518c976b03a8e1cd4eeb7e4a71de1875e87
A01-1-0603-21 <A01-1-0603-21@miremington.local>
2023-09-30 14:46:09 -0500

    operadores y clases

---
+++ javascript/templates.js
@@ -0,0 +1,7 @@
+let estudiante = {
+    nombre: "Sebatian",
+    apellido: "Cualquiera",
+    id: 1234
+}
+
+console.log(`${estudiante.esProfesor} Hola ${estudiante.nombre}. Tu id es ${estudiante.id}`);
